4308deb3c06396f515e6b3284b1d35d45937c4f2
Braden Heeney <bheeney97@gmail.com>
2024-12-19 21:57:17 -0700

    Update test.java

--- Module2/test.java
+++ Module2/test.java
@@ -1,3 +1,9 @@
-public test{
-    
+
+public class test{
+    public static void main(String[] args) {
+        int[] numList = {10,4,7,18,29,34,19};
+        for (int i =0; i < numList.length; i++){
+            System.err.println(numList[i]);
+        }
+    }
 }
