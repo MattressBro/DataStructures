baea6ecd05b5fb77038539d62041485cda373292
Braden Heeney <bheeney97@gmail.com>
2025-01-10 15:27:20 -0700

    Create discussion.java

---
+++ Module4/discussion.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+
+public class discussion {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println("Stack: " + stack);
+
+        System.out.println("Pop: " + stack.pop());
+        System.out.println("Stack: " + stack);
+
+        System.out.println("Peek: " + stack.peek());
+        System.out.println("Stack: " + stack);
+
+        System.out.println("Search for 2: " + stack.search(2));
+        System.out.println("Search for 3: " + stack.search(3));
+        System.out.println("Search for 1: " + stack.search(1));
+        System.out.println("Search for 4: " + stack.search(4));
+    }
+}
