bb85c4dbd83114609475e65f557f59a459a31238
Braden Heeney <bheeney97@gmail.com>
2025-01-12 16:08:27 -0700

    asd

--- Module4/module4/postfix.java
+++ Module4/module4/postfix.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.Stack;
 public class postfix {
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\onesh\\OneDrive\\Documents\\GitHub\\DataStructures\\Module4\\module4postFix.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\onesh\\OneDrive\\Documents\\GitHub\\DataStructures\\Module4\\module4\\postFix.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println("Postfix expression: " + line);
@@ -15,19 +15,6 @@ public class postfix {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String expression = "42*3+";
-        System.out.println("Postfix expression: " + expression);
-        System.out.print("Postfix expression result: ");
-        parsePostfix(expression);
-        String expression2 = "53+7*";
-        System.out.println("Postfix expression: " + expression2);
-        System.out.print("Postfix expression result: ");
-        parsePostfix(expression2);
-        String expression3 = "42*+"; // Missing operand
-        System.out.println("Postfix expression: " + expression3);
-        System.out.print("Postfix expression result: ");
-        parsePostfix(expression3);
-
     }
     public static void parsePostfix(String expression){
         Stack<Integer> stack = new Stack<>();
