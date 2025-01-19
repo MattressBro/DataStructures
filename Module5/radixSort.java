864e48e6012fabd99b5f4d69dab0857623d88deb
Braden Heeney <bheeney97@gmail.com>
2025-01-19 16:10:02 -0700

    asd

--- Module5/radixSort.java
+++ Module5/radixSort.java
@@ -1,8 +1,11 @@
 package Module5;
-
+//Braden Heeney
+//CS400-1
+//1/19/2025
+//Radix Sort
 public class radixSort {
     //method to find highest number in array to 
-    //then discover the max digits
+    //then discover the max place value
     private static int maxNum(int[] arr){
         int max = arr[0];
         for(int num : arr){
@@ -12,43 +15,37 @@ public class radixSort {
         }
         return max;
     }
-    public static void radix(int[] arr){
-        int max = maxNum(arr);
-        System.out.println("Max Number "+max);
-        int digiMax = (int)Math.log10(max)+1;
-        System.out.println("Max digits "+digiMax);
-        int place =1;
-        for(int i=0; i < digiMax; i++){
-            sort(arr,place);
-            place *=10;
-            for (int num : arr) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-        }
+    private static void sort(int[] arr, int place, int max) {
+    //base case for recursion will trigger when place value is greater than max number in array:  783/1000 = 0
+    if (max / place <= 0) {
+        return;
     }
+    bucketSort(arr, place);
+    sort(arr, place * 10, max);//recursive call to sort array place value is mutliplied by 10 - 1*10 gets your to the tens place then hundreds etc...
+    }
+    //bucketSort method is called for every place value
+    private static void bucketSort(int[] arr, int place) {
+        int n = arr.length;
+        int[][] buckets = new int[10][n];//2D array to hold all buckets
+        int[] bCount = new int[10];
 
-    public static void sort(int[] arr, int place){
-        for(int i=0; i < arr.length-1; i++){
-            for(int j=i+1; j < arr.length; j++){
-                int digitI =(arr[i] / place )% 10;
-                int digitJ =(arr[j] / place )% 10;
-                if(digitI > digitJ){
-                    int temp =arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+        for (int i = 0; i < n; i++) {
+            int bIndex = (arr[i] / place) % 10;
+            buckets[bIndex][bCount[bIndex]++] = arr[i];
+        }
+
+        int index = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < bCount[i]; j++) {
+                arr[index++] = buckets[i][j];
             }
         }
     }
-    public static void main(String[] args){
-        //int[] arr = {543, 356, 264, 472, 783, 182, 692, 94, 295, 491, 99};
+        public static void main(String[] args){
         int[] arr = {783,99,472,182,264,543,356,295,692,491,94};
-        radix(arr);
-        //sort(arr, 100);
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+        int max = maxNum(arr);
+        System.out.println("Original array: "+ java.util.Arrays.toString(arr));
+        sort(arr, 1, max);
+        System.out.println("Sorted array:   "+ java.util.Arrays.toString(arr));
     }
 }
-//int[] arr = {783,99,472,182,264,543,356,295,692,491,94};
